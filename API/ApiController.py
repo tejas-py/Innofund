ae616df92068c4b0bd9c15c4de6cbc46e2ab20ae
tejasLucifer <tejas.parikh1@s.amity.edu>
2022-03-07 12:08:09 +0530

    initial

--- API/ApiController.py
+++ API/ApiController.py
@@ -1,23 +1,16 @@
 from flask import Flask
 import transcations.CreateAccount as trans
 from API import connection
-
-from utilities.CommonFunctions import get_private_key_from_mnemonic
+from utilities.accounts import account
 
 app = Flask(__name__)
 
 algod_client = connection.algo_conn()
 
 
-def account123():
-    account1 = "couch unveil crush drastic staff peanut tooth code matrix drip jazz team ticket ring punch local patrol smile stay hobby maze swamp cash absent erupt"
-    a1 = get_private_key_from_mnemonic(account1)
-    return a1
-
-
 @app.route('/createAccount/<string:name/string:usertype>', methods=["POST"])
 def create_account(name, usertype):
-    appId = trans.create_app(algod_client, account123(), name, usertype)
+    appId = trans.create_app(algod_client, account(), name, usertype)
     return appId
 
 
