3e683b82cb36b3d44839ff9ca0e62ab46d0d8707
tejasLucifer <tejas.parikh1@s.amity.edu>
2022-03-07 12:15:34 +0530

    initial

--- API/ApiController.py
+++ API/ApiController.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 algod_client = connection.algo_conn()
 
 
-@app.route('/createAccount/<string:name/string:usertype>', methods=["POST"])
+@app.route('/createAccount/<string:name>/<string:usertype>', methods=["POST"])
 def create_account(name, usertype):
     appId = trans.create_app(algod_client, account(), name, usertype)
     return appId
