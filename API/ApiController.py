34dbc34ad5c9156b66d7c9120571d9f2e5b4cfcc
tejasLucifer <tejas.parikh1@s.amity.edu>
2022-03-07 14:42:43 +0530

    initial

--- API/ApiController.py
+++ API/ApiController.py
@@ -7,13 +7,23 @@ app = Flask(__name__)
 algod_client = API.connection.algo_conn()
 
 
-@app.route('/createAccount/<string:name>/<string:usertype>/<string:email>/<string:password>', methods=["POST"])
+@app.route('/createAccount/'
+           '<string:name>'
+           '/<string:usertype>'
+           '/<string:email>'
+           '/<string:password>',
+           methods=["POST"])
 def create_account(name, usertype, email, password):
     appId = trans.create_app(algod_client, name, usertype, email, password)
     return appId
 
 
-@app.route('/createAccount/<string:title>/<string:description>/<string:fund_limit>/<string:duration>', methods=["POST"])
+@app.route('/createCampaign/'
+           '<string:title>'
+           '/<string:description>'
+           '/<string:fund_limit>'
+           '/<string:duration>',
+           methods=["POST"])
 def create_account(title, description, fund_limit, duration):
     appId = trans.create_app(algod_client, title, description, fund_limit, duration)
     return appId
