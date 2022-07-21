f9390762059c8bbb8cd56506d77ab78116f3c00f
tejas <tejas@webmobinfo.ch>
2022-07-21 11:27:11 +0530

    Reward Milestone Close out asset

--- Contracts/campaign/campaign_contract.py
+++ Contracts/campaign/campaign_contract.py
@@ -110,7 +110,7 @@ def approval_program():
             TxnField.type_enum: TxnType.AssetTransfer,
             TxnField.asset_receiver: Txn.accounts[1],
             TxnField.asset_close_to: Txn.accounts[1],
-            TxnField.asset_amount: Int(1),
+            TxnField.asset_amount: Int(0),
             TxnField.xfer_asset: Txn.assets[0],
             TxnField.fee: Int(0)
         }),
